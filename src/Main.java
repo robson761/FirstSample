9905dfd9afd018f0292e08d03e59145dd57016ec
Robert <rbalcewicz@o2.pl>
2018-04-30 20:03:23 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int x;
+        int a;
+//        int y;
+        x = 4;
+        a = 2;
+
+        double y = Math.pow(x, a);
+
+        System.out.println(y);
+    }
+}
